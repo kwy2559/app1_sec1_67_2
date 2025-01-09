785cb0d21c8658316613258b09cb58f02d88c2d9
kwy2559 <152370972+kwy2559@users.noreply.github.com>
2025-01-09 11:27:50 +0700

    add func-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import { Header,Content,Footer } from './func-components';
 import './style.css'
 function App() {
   
   return (
-    <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
-    </>
-  )
+    [<Header/>,<Content/>,<Footer/>]
+   )
 }
 
 export default App;
