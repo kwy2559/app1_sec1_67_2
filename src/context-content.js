be5375e3f0c08ea6f32a0d1158538427db0cd558
kwy2559 <152370972+kwy2559@users.noreply.github.com>
2025-01-30 10:14:25 +0700

    create context

---
+++ src/context-content.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { userContext } from "./context";
+
+export default function Content(){
+    let user =  React.useContext(userContext)
+
+    const ContentStyle={
+        backgroundColor:'#ddd',
+        textAlign:'center',
+        margin:10,
+        padding:10
+    }
+    return(
+        <div style={ContentStyle}>
+            Hello{user}
+        </div>
+    )
+}
